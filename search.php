2539a0010b68cd320506e0a00f4d7c2201fa637d
nonalely <230605110193@student.uin-malang.ac.id>
2025-06-29 05:59:55 +0700

    search.php

--- search.php
+++ search.php
@@ -8,8 +8,10 @@ $hasil = mysqli_query($conn, "SELECT * FROM artikel WHERE judul LIKE '%$q%' ORDE
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <title>Hasil Pencarian - WebArtikel</title>
+    <title>Hasil Pencarian - Web Artikel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 
