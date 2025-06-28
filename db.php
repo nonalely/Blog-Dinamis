689406fd5e5f70c48745f5a502e4f30ce70f7540
nonalely <230605110193@student.uin-malang.ac.id>
2025-06-29 05:48:37 +0700

    db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "webartikel";
+$host = "sql200.infinityfree.com";
+$user = "if0_39347016";
+$pass = "dDxJgR11XI";
+$db = "if0_39347016_webartikel";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
