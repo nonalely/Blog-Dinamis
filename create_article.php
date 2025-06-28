d932ee1faa1fcf05075169ab038b7d76e79f3651
nonalely <230605110193@student.uin-malang.ac.id>
2025-06-29 05:50:51 +0700

    create_article.php

--- create_article.php
+++ create_article.php
@@ -29,7 +29,10 @@ $kategori = mysqli_query($conn, "SELECT * FROM kategori");
 <head>
     <meta charset="UTF-8">
     <title>Tambah Artikel - WebArtikel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body class="bg-light">
 <div class="container mt-5">
